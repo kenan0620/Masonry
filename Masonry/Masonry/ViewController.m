209a9d5fe3d072fc3db78022318af0e0f4a0f200
conan <houkenan0620@126.com>
2017-02-21 09:26:21 +0800

    170221、Masonry

--- Masonry/Masonry/ViewController.m
+++ Masonry/Masonry/ViewController.m
@@ -7,18 +7,88 @@
 //
 
 #import "ViewController.h"
-
+#import <Masonry.h>
+#import "UIView+ConanViewLayer.h"
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-
+{
+    UIButton *masonryBtn;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self creatBtn];
+    
+}
+#pragma mark ~~~~~~~~~creatView
+-(void)creatView
+{
+    
+}
+#pragma mark ~~~~~~~~~creatLab
+-(void)creatLab
+{
+    
+}
+#pragma mark ~~~~~~~~~creatBtn
+-(void)creatBtn
+{
+    masonryBtn=[UIButton buttonWithType:UIButtonTypeCustom];
+    [masonryBtn addTarget:self action:@selector(masonryBtnChick) forControlEvents:UIControlEventTouchUpInside];
+    [masonryBtn ConanMakeCornerRadius:50 BorderColor:[UIColor yellowColor] BorderWidth:12 BackgroundColor:[UIColor brownColor]];
+    UIEdgeInsets masonryBtnEdgeInsets = UIEdgeInsetsMake(20, 20, 20, 20);
+    [self.view addSubview:masonryBtn];
+    
+    [masonryBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view).with.insets(masonryBtnEdgeInsets);
+
+    }];
+   
 }
 
+-(void)masonryBtnChick
+{
+    NSLog(@"masonryBtnChick");
+     UIEdgeInsets masonryBtnEdgeInsets = UIEdgeInsetsMake(20, 100, 200, 20);
+    [masonryBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view).with.insets(masonryBtnEdgeInsets);
+        
+    }];
+    
+}
+
+#pragma mark ~~~~~~~~~creatImageView
+-(void)creatImageView
+{
+    
+}
+
+#pragma mark ~~~~~~~~~creatTableView
+-(void)creatTableView
+{
+    
+}
+
+#pragma mark ~~~~~~~~~creatScrollView
+-(void)creatScrollView
+{
+    
+}
+
+#pragma mark ~~~~~~~~~creatTextFiled
+-(void)creatTextFiled
+{
+    
+}
+
+#pragma mark ~~~~~~~~~createColletionView
+-(void)ColletionView
+{
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
